f68e22f009466ac6563029be541d09b4028e0c33
DoxenNYC <jrdoxen@gmail.com>
2019-02-13 20:56:49 -0600

    complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FriendFinder-App
+
+1. Two separate HTML pages: home and the questionaire page.
+2. On the questionaire page, enter personal info and answer ten questions to be recommended a friend.
+3. After questions have been answered, press submit and user will receive the closest match for a friend from the friends list from the API.
---
+++ data/friends.js
@@ -0,0 +1,70 @@
+// FRIENDS API DATA
+
+var friendsArray = [
+    {
+        name: "Alexis",
+        photo: "https://us.123rf.com/450wm/varka/varka1705/varka170500365/78878059-stock-vector-african-american-pretty-girl-portrait-black-beauty-concept-vector-illustration-of-black-woman-great-.jpg?ver=6",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Courtnie",
+        photo: "https://us.123rf.com/450wm/aunaauna/aunaauna1606/aunaauna160600251/59069104-stock-vector-flat-female-face-avatar-of-smiling-pretty-african-american-young-girl-.jpg?ver=6",
+        scores: [
+            4,
+            2,
+            5,
+            1,
+            3,
+            2,
+            2,
+            1,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Jenni",
+        photo: "https://media.istockphoto.com/vectors/woman-front-face-cartoon-avatar-icon-vector-id613880878?k=6&m=613880878&s=612x612&w=0&h=5RzNun1RryqR8CVsbY5GlCl-ahbZth4WdsOi-tWIou8=",
+        scores: [
+            5,
+            2,
+            2,
+            2,
+            4,
+            1,
+            3,
+            2,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Drea",
+        photo: "https://thumbs.dreamstime.com/z/pretty-young-brunette-latina-mexican-woman-smiling-face-expression-avatar-105320579.jpg",
+        scores: [
+            3,
+            3,
+            4,
+            2,
+            2,
+            1,
+            3,
+            2,
+            2,
+            3
+        ]
+    }
+];
+
+module.exports = friendsArray;
